39cf182fd2e4ba934154972fcb0eeeb2a6e7796e
2018130 <35947698+2018130@users.noreply.github.com>
2026-02-13 16:27:51 +0900

    [Add] Apply remember piece gimmic

---
+++ Assets/3.Script/PuzzleGimmic/RememberPiece.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RememberPiece : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    private Sprite backPieceImage;
+
+    private Sprite originImage;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originImage = spriteRenderer.sprite;
+    }
+
+    public void ChangeToBackImage()
+    {
+        spriteRenderer.sprite = backPieceImage;
+    }
+
+    private void OnDestroy()
+    {
+        originImage = null;
+        backPieceImage = null;
+    }
+
+    public void EndInteract()
+    {
+
+    }
+
+    public void Interact(Vector2 worldPosFromMousePosition)
+    {
+        if(spriteRenderer.sprite == backPieceImage)
+        {
+            spriteRenderer.sprite = originImage;
+        }
+    }
+}
